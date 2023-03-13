dab501ae3d3da5c29298d4d7ef6a1e42bebb5895
pavan <pavankumartule143@gmail.com>
2023-03-13 16:11:25 +0530

    browser token created

--- nse_ass/nse.py
+++ nse_ass/nse.py
@@ -3,3 +3,4 @@ from bs4 import BeautifulSoup
 
 url = "https://www1.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?symbolCode=-9999&symbol=BANKNIFTY&symbol=BANKNIFTY&instrument=OPTIDX&date=-&segmentLink=17&segmentLink=17p"
 
+headers = {'User-Agent': 'Mozilla/5.0'}
