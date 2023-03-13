b0ebabeadb12146b80d0bbff4cedc4842ac7cecf
pavan <pavankumartule143@gmail.com>
2023-03-13 16:10:22 +0530

    BANKNIFTY url created

--- nse_ass/nse.py
+++ nse_ass/nse.py
@@ -1,2 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
+
+url = "https://www1.nseindia.com/live_market/dynaContent/live_watch/option_chain/optionKeys.jsp?symbolCode=-9999&symbol=BANKNIFTY&symbol=BANKNIFTY&instrument=OPTIDX&date=-&segmentLink=17&segmentLink=17p"
+
